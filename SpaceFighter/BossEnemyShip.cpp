bb54a67c45721ff9f16fd0e5f183fb2b5271fb92
TQuinn2135 <Tquinn2135@gmail.com>
2024-12-10 10:18:31 -0600

    Tweaked the BossEnemyShip

--- SpaceFighter/BossEnemyShip.cpp
+++ SpaceFighter/BossEnemyShip.cpp
@@ -3,19 +3,19 @@
 
 BossEnemyShip::BossEnemyShip()
 {
-	SetSpeed(100); // change this value to change speed. Make adjustments as needed 
-	SetMaxHitPoints(3); //change the hit points
-	SetCollisionRadius(120);
+	SetSpeed(90); // change this value to change speed. Make adjustments as needed //tweaked last by tom
+	SetMaxHitPoints(25); //change the hit points //tweaked last by tom
+	SetCollisionRadius(125); //don't change
 }
 
 void BossEnemyShip::Update(const GameTime& gameTime)
 {
 	if (IsActive())
 	{
-		float x = sin(gameTime.GetTotalTime() * Math::PI + GetIndex());
+		float x = sin(gameTime.GetTotalTime() * Math::PI_OVER2 + GetIndex());
 		x *= GetSpeed() * gameTime.GetElapsedTime() * 1.4f;
-		TranslatePosition(x, GetSpeed() * gameTime.GetElapsedTime());
-
+		TranslatePosition(x * 1.5, GetSpeed() * gameTime.GetElapsedTime()); //added the PI_OVER2 and the * 1.5 to TranslatePosition in order to
+																			//to give the boss a wider range of movement
 		if (!IsOnScreen()) Deactivate();
 	}
 
